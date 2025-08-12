f1bb7a0075f7d26bf69ada621c7da275cc7360fe
saad0x1 <149149348+saad0x1@users.noreply.github.com>
2025-08-12 14:02:31 -0400

    Add BloodHound

---
+++ connectors/bloodhound-python.py
@@ -0,0 +1,47 @@
+from .base import Module, register_module, sub_module
+
+
+@register_module
+class BloodHound(Module):
+    name = "Blood Hound Collection"
+    description = ""
+
+   
+    domain: str = ""
+    username: str = ""
+    password: str = ""
+    kerberos: bool = False
+
+    @sub_module("Collection  (BloodHound.py)")
+    def run_bloodhoundpy(
+        self,
+        domain: str = "",
+        username: str = "",
+        password: str = "",
+        nameserver: str= "",
+        kerberos: bool = False,
+    ) -> str:
+        if kerberos:
+            return f"bloodhound-python -u '{username}' -d '{domain}' -k -c all -v -ns {nameserver}"
+        return f"bloodhound-python -u '{username}' -d '{domain}' -p '{password}' -c all -v -ns {nameserver}"
+
+    @sub_module("Collection (NetExec)")
+    def run_netexec(
+        self,
+        domain: str = "",
+        username: str = "",
+        password: str = "",
+        kerberos: bool = False,
+    ) -> str:
+        if kerberos:
+            return f"nxc ldap '{domain}' -u '{username}' -k --bloodhound --collection All'"
+        return f"nxc ldap '{domain}' -u '{username}' -p '{password}' --bloodhound --collection All"
+        
+
+    @sub_module("Collection (SharpHound)")
+    def run_sharphound(
+        self,
+        output: str = "",
+    ):
+        return f"SharpHound.exe --CollectionMethods All --ZipFileName {output}.zip"
+
